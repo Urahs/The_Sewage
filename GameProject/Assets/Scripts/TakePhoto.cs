d3490bc8c994ff7c3f3b5658d71d131171c2abde
b21727685 <b21727685@cs.hacettepe.edu.tr>
2021-05-09 15:41:56 +0300

    Foto Çekimi

---
+++ GameProject/Assets/Scripts/TakePhoto.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakePhoto : MonoBehaviour
+{
+    public ScreenShotCamera snapCam;
+    
+
+    TextureImporter importer;
+    public RawImage rawImage;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)){
+            snapCam.CallTakeSnapShot();
+            Invoke("CallmePls", 2.0f);
+        }
+    }
+
+    void CallmePls(){
+        
+        UnityEditor.AssetDatabase.Refresh();
+        
+        rawImage.texture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Snapshots/resim00.png", typeof(Texture2D));
+        if(rawImage.texture == null)
+            Debug.Log("Raw Image Null Error!");
+        
+
+        /*
+        importer = (TextureImporter)TextureImporter.GetAtPath("Assets/snapShots/resim00.png");
+        importer.textureType = TextureImporterType.Sprite;
+
+        Invoke("CallmeAgain", 2.0f);
+        */
+
+    }
+
+    /*
+    void CallmeAgain(){
+        //UnityEditor.AssetDatabase.Refresh();
+        photo.sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Snapshots/resim00.png");
+    }
+    */
+}
